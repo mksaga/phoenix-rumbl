50d96f41f48fcf1a7c9676c30e4087dcfeb907d1
Mohamed Aly <mksaga@hey.com>
2022-03-07 12:21:16 -0500

    Add login to accounts context

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -8,4 +8,13 @@ defmodule RumblWeb.Auth do
     user = user_id && Rumbl.Accounts.get_user(user_id)
     assign(conn, :current_user, user)
   end
+
+  def login(conn, user) do
+    conn
+    # use the "assigns" field in Plug.Conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    # protect us from fixation attacks: send session cookie to client with new identifier
+    |> configure_session(renew: true)
+  end
 end
